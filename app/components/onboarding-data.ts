8fb6b6a4759388af53e3fb304b2a6389f2c92419
Jintu Pradeep <jintupradeep@gmail.com>
2023-07-03 20:38:11 +0530

    Add onboarding-data to components folder

---
+++ app/components/onboarding-data.ts
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+const onboardingData = [
+  {
+    id: 1,
+    title: 'Unlimited Entertainment',
+    description: 'Stream movies, TV shows, and documentaries anytime, anywhere.',
+    image: require('../assets/best-streaming.jpg'),
+  },
+  {
+    id: 2,
+    title: 'Personalized Recommendations',
+    description: 'Get personalized recommendations based on your watching history.',
+    image: require('../assets/best-streaming.jpg'),
+  },
+  {
+    id: 3,
+    title: 'Download and Watch Offline',
+    description: 'Download your favorite shows and watch them offline on the go.',
+    image: require('../assets/best-streaming.jpg'),
+  },
+];
+export default onboardingData;
