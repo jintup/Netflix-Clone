e77f6cd6105508d61a65eb78a002c738e0c3ad7a
Jintu Pradeep <jintupradeep@gmail.com>
2023-07-03 20:31:24 +0530

    Add long-button.tsx to component folder

---
+++ app/components/long-button.tsx
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const Lbutton = ({ title, onPress, color }) => {
+
+    const buttonStyle = [styles.button, { backgroundColor: color }]
+    return (
+        <TouchableOpacity style={buttonStyle} onPress={onPress}>
+            <Text style={styles.buttonText}>{title}</Text>
+        </TouchableOpacity>
+    )
+}
+export default Lbutton;
+const styles = StyleSheet.create({
+    button: {
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+})
