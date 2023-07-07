1bd429c1087dfe160b3a8c84167a9cfe739c9b5e
Jintu Pradeep <jintupradeep@gmail.com>
2023-07-07 20:05:50 +0530

    Add a logo to assets folder and toursrn-header file to components

---
+++ app/components/header.tsx
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, Image, View } from 'react-native';
+import React from 'react';
+
+const Header = () => {
+    return (
+        <View>
+            <View style={styles.container}>
+                <Image
+                    source={require('../assets/nlogo.png')}
+                    style={styles.logo}
+                    resizeMode="contain"
+                />
+            </View>
+        </View>
+    );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+        paddingTop: 20,
+    },
+    logo: {
+        width: 100,
+        height: 40,
+    },
+    logoContainer: {
+        position: 'absolute',
+        top: 20,
+        right: 20,
+    },
+    logoImage: {
+        width: 50,
+        height: 50,
+        resizeMode: 'contain',
+    },
+});
