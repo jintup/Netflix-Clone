3c35782f396de9fd50a0b1fc8709b8c051866b1c
Jintu Pradeep <jintupradeep@gmail.com>
2023-07-11 18:07:53 +0530

    header component modified

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,42 +1,37 @@
-import { StyleSheet, Text, Image, View } from 'react-native';
+import { StyleSheet, Text, Image, View, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { Colors } from '../theme/colors';
 
-const Header = () => {
-    return (
-        <View>
-            <View style={styles.container}>
-                <Image
-                    source={require('../assets/images/nlogo.png')}
-                    style={styles.logo}
-                    resizeMode="contain"
-                />
-            </View>
-        </View>
-    );
+const Header: React.FC = ({ leftImageSource, rightImageSource }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={leftImageSource} style={styles.leftLogo} resizeMode="contain" />
+      <Image source={rightImageSource} style={styles.rightLogo} resizeMode="contain" />
+    </View>
+  );
 };
 
 export default Header;
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingHorizontal: 20,
-        paddingTop: 20,
-    },
-    logo: {
-        width: 100,
-        height: 40,
-    },
-    logoContainer: {
-        position: 'absolute',
-        top: 20,
-        right: 20,
-    },
-    logoImage: {
-        width: 50,
-        height: 50,
-        resizeMode: 'contain',
-    },
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 100,
+    //backgroundColor: Colors.darkblack,
+  },
+  leftLogo: {
+    width: 100,
+    height: 50,
+    marginLeft: 10,
+  },
+  rightLogo: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
+    borderRadius: 6,
+  },
 });
+
+export default Header;
