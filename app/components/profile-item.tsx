472220d09543aabeb6a89cec9ccbe4b5b029ab9b
Jintu Pradeep <jintupradeep@gmail.com>
2023-07-17 20:09:01 +0530

    added a new component called 'Profile-item' to represent the profileimages and name

---
+++ app/components/profile-item.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
+
+const ProfileItem: React.FC = ({profile, onPress}) => {
+  return (
+    <TouchableOpacity style={styles.profile} onPress={onPress}>
+      <View style={styles.profileImageContainer}>
+        <Image style={styles.profileImage} source={profile.image} />
+        <Text style={styles.profileName}>{profile.name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  profile: {
+    marginRight: 40,
+    left: 20,
+  },
+  profileImageContainer: {
+    alignItems: 'center',
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 5,
+  },
+  profileName: {
+    fontSize: 16,
+    padding: 10,
+    marginLeft: 15,
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+});
+
+export default ProfileItem;
