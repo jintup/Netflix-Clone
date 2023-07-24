ded71e0b6a5a192d02a4a76feca67199a2b2a8c0
Jintu Pradeep <jintupradeep@gmail.com>
2023-07-24 21:26:26 +0530

    new style added for button

--- app/screens/profile-screen.tsx
+++ app/screens/profile-screen.tsx
@@ -1,36 +1,36 @@
 import React from 'react';
-import {useNavigation} from '@react-navigation/native';
-import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import CHeader from '../components/centerheader';
 import ProfileItem from '../components/profile-item';
 import Ptext from '../components/profile-text';
 
 const ProfileScreen: React.FC = () => {
   const profiles = [
-    {name: 'John', image: require('../assets/images/avatar.png')},
-    {name: 'Children', image: require('../assets/images/child.jpg')},
+    { name: 'John', image: require('../assets/images/avatar.png') },
+    { name: 'Children', image: require('../assets/images/child.jpg') },
   ];
   const navigation = useNavigation();
 
-  const handleProfilePress = profileName => {
+  const handleProfilePress = (profileName) => {
     if (profileName === 'John') {
       navigation.navigate('Home');
     } else if (profileName === 'Children') {
-      navigation.navigate('Home');
+      navigation.navigate('ChildrenHome');
     }
   };
   return (
     <View style={styles.headercontainer}>
       <CHeader />
       <View style={styles.container}>
-      <Ptext text="Who is watching?" />
+        <Ptext text="Who is watching?" />
         <View style={styles.profilesContainer}>
           {profiles.map((profile, index) => (
             <ProfileItem
-            key={index}
-            profile={profile}
-            onPress={() => handleProfilePress(profile.name)}
-          />
+              key={index}
+              profile={profile}
+              onPress={() => handleProfilePress(profile.name)}
+            />
           ))}
         </View>
       </View>
