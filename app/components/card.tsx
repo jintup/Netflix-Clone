0ab45a96d43b032d04cad6141957ba0fd05ba332
Jintu Pradeep <jintupradeep@gmail.com>
2023-08-01 01:41:13 +0530

    fixed type errors

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, FlatList } from 'react-native';
 import axios from '../services/axios';
 import { API_KEY } from '../constants/api-constant';
 import { imageUrl } from '../services/api-config';
-import YoutubePlayer from 'react-native-youtube-iframe';
 import { useNavigation } from '@react-navigation/native';
 import { horizontalScale, moderateScale, verticalScale } from '../utils/scale';
-const MovieCard: React.FC = ({ title, url }) => {
-  const [movies, setMovies] = useState([]);
-  const [urlId, setUrlId] = useState('');
+
+interface Movie {
+  id: number;
+  original_title: string;
+  overview: string;
+  backdrop_path: string;
+}
+
+const MovieCard: React.FC<{ title: string; url: string }> = ({ title, url }) => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [urlId, setUrlId] = useState<string>('');
   const navigation = useNavigation();
+
   useEffect(() => {
     axios
       .get(url)
@@ -17,20 +25,18 @@ const MovieCard: React.FC = ({ title, url }) => {
         // console.log(response.data);
         setMovies(response.data.results);
       })
-      .catch(
-        (err) => {
-          console.log(err);
-        },
-        [url],
-      );
-  });
-  const handleMovie = (id, original_title, overview) => {
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [url]);
+
+  const handleMovie = (id: number, original_title: string, overview: string) => {
     axios
       .get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
       .then((response) => {
         if (response.data.results.length !== 0) {
-          setUrlId(response.data.results[0]);
-          navigation.navigate('VideoScreen', { videoId: urlId.key, original_title, overview });
+          setUrlId(response.data.results[0].key);
+          navigation.navigate('VideoScreen', { videoId: urlId, original_title, overview });
         } else {
           console.log('Array empty, video not available');
         }
@@ -39,25 +45,25 @@ const MovieCard: React.FC = ({ title, url }) => {
         console.log('Error fetching video:', error.message);
       });
   };
+
+  const renderItem = ({ item }: { item: Movie }) => (
+    <View style={styles.imageWrapper} key={item.id}>
+      <TouchableOpacity onPress={() => handleMovie(item.id, item.original_title, item.overview)}>
+        <Image style={styles.image} source={{ uri: imageUrl + item.backdrop_path }} />
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
     <View>
       <Text style={styles.title}>{title}</Text>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        <View style={styles.imageContainer}>
-          {movies &&
-            movies.map(
-              (
-                obj, // Add a conditional check for 'movies' array
-              ) => (
-                <View style={styles.imageWrapper} key={obj.id}>
-                 <TouchableOpacity onPress={() => handleMovie(obj.id, obj.original_title, obj.overview)}>
-                  <Image style={styles.image} source={{ uri: imageUrl + obj.backdrop_path }} />
-                </TouchableOpacity>
-                </View>
-              ),
-            )}
-        </View>
-      </ScrollView>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={movies}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
 };
@@ -67,13 +73,6 @@ const styles = StyleSheet.create({
     fontSize: moderateScale(19),
     fontWeight: 'bold',
     color: 'white',
-  },
-  imageContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    margin: horizontalScale(10),
-    borderRadius: 6,
-    overflow: 'hidden',
   },
   imageWrapper: {
     marginRight: horizontalScale(10),
--- app/components/child-card.tsx
+++ app/components/child-card.tsx
@@ -1,26 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import {
-  ScrollView,
-  StyleSheet,
-  Text,
   View,
+  Text,
   Image,
   TouchableOpacity,
+  FlatList,
   Dimensions,
+  StyleSheet,
 } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMovies, fetchMovieVideos, clearMovies } from '../store/feature/video/movieSlice';
 import { imageUrl } from '../services/api-config';
-// import YoutubePlayer from 'react-native-youtube-iframe';
 import { useNavigation } from '@react-navigation/native';
 import { horizontalScale, moderateScale, verticalScale } from '../utils/scale';
 
-const ChildCard = ({ title, url }) => {
+interface Movie {
+  id: number;
+  original_title: string;
+  overview: string;
+  backdrop_path: string;
+}
+
+const ChildCard: React.FC<{ title: string; url: string }> = ({ title, url }) => {
   const dispatch = useDispatch();
-  const { movies, urlId } = useSelector((state) => state.movies);
+  const { movies, urlId } = useSelector((state: RootState) => state.movies);
   const screen = Dimensions.get('screen');
   const navigation = useNavigation();
-  // const [selectedVideoId, setSelectedVideoId] = useState(null);
 
   useEffect(() => {
     dispatch(fetchMovies(url));
@@ -29,10 +34,8 @@ const ChildCard = ({ title, url }) => {
     };
   }, [dispatch, url]);
 
-  const handleMovie = (id,original_title, overview) => {
+  const handleMovie = (id: number, original_title: string, overview: string) => {
     dispatch(fetchMovieVideos(id)).then(() => {
-      // console.log('Fetched video ID:', urlId);
-      // setSelectedVideoId(urlId);
       if (urlId) {
         navigation.navigate('VideoScreen', {
           videoId: urlId,
@@ -45,21 +48,24 @@ const ChildCard = ({ title, url }) => {
     });
   };
 
+  const renderMovieItem = ({ item }: { item: Movie }) => (
+    <View style={styles.imageWrapper}>
+      <TouchableOpacity onPress={() => handleMovie(item.id, item.original_title, item.overview)}>
+        <Image style={styles.image} source={{ uri: imageUrl + item.backdrop_path }} />
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
     <View>
       <Text style={styles.title}>{title}</Text>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        <View style={styles.imageContainer}>
-          {movies.map((obj) => (
-            <View style={styles.imageWrapper} key={obj.id}>
-              <TouchableOpacity
-                onPress={() => handleMovie(obj.id, obj.original_title, obj.overview)}>
-                <Image style={styles.image} source={{ uri: imageUrl + obj.backdrop_path }} />
-              </TouchableOpacity>
-            </View>
-          ))}
-        </View>
-      </ScrollView>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={movies}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderMovieItem}
+      />
       {/* {selectedVideoId && (
         <YoutubePlayer height={screen.height} videoId={selectedVideoId} play={true} />
       )} */}
@@ -74,7 +80,6 @@ const styles = StyleSheet.create({
     color: 'white',
   },
   imageContainer: {
-    flex: 1,
     flexDirection: 'row',
     margin: horizontalScale(10),
     borderRadius: 6,
