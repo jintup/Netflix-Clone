f28622c335d33d962266a3786a0b75e1f63c101f
Jintu Pradeep <jintupradeep@gmail.com>
2023-08-06 21:59:23 +0530

    splashscreen added

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navigation from './app/navigators/navigation';
 import { Provider } from 'react-redux';
 import store from './app/store/root-store/store';
+import RNBootSplash from 'react-native-bootsplash';
 
 const App: React.FC = () => {
+  const hideSplashScreen = RNBootSplash.hide;
+  useEffect(() => {
+    const init = async () => {};
+    init().finally(async () => {
+      await hideSplashScreen({ fade: true, duration: 50 });
+    });
+  }, []);
   return (
     <Provider store={store}>
       <Navigation />
@@ -12,4 +20,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
